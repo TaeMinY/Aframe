d9040151e9345ffcb2e4d54371cc4ed6f9ef77f4
태민 <48385263+UniTaeMin@users.noreply.github.com>
2019-11-25 22:15:15 +0900

    Hav

--- src/components/docs/0.9.0/Nav.jsx
+++ src/components/docs/0.9.0/Nav.jsx
@@ -21,11 +21,29 @@ margin: 3px 0px;
 
 `
 
+const Img = styled.img`
+    width:15px;
+    height:15px;
+    transition:0.2s;
+    transform : ${props => ((props.state === true) ? 'rotate(90deg)' : 'rotate(-90deg)')};
+`
+
+
+
 const Tags = ["<a-box>", "<a-camera>", "<a-circle>", "<a-cone>", "<a-cursor>", "<a-curvedimage>", "<a-cylinder>", "<a-dodecahedron>", "<a-gltf-model>", "<a-icosahedron>", "<a-image>", "<a-light>", "<a-link>", "<a-obj-model>", "<a-octahedron>", "<a-plane>", "<a-ring>", "<a-sky>", "<a-sound>", "<a-sphere>", "<a-tetrahedron>", "<a-text>", "<a-torus-knot>", "<a-torus>", "<a-trianle>", "<a-video>", "<a-videosphere>"]
+
 
 function Nav() {
 
+    const [tagState, setTagState] = React.useState(false);
+
     const test = [() => { console.log("a") }, () => { console.log("b") }]
+
+
+
+    const change = () => {
+        setTagState(!tagState)
+    }
 
     return (
         <Wrapper>
@@ -34,15 +52,22 @@ function Nav() {
                     width: "70%",
                     margin: "10% auto",
                 }}>
-                    <Subject>기본 태그</Subject>
-                    <div style={{ margin: "5px 10px" }}>
-                        {(Tags || []).map((value, index) => (
-                            <Tag className="Tag" onClick={test[index]} >{value}</Tag>
-                        ))}
+                    <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between", transition: "0.2s" }} onClick={change}>
+                        <Subject>기본 태그</Subject>
+                        <Img src="/arrow.svg" state={tagState} ></Img>
                     </div>
+                    {
+                        tagState === true ? (
+                            <div style={{ margin: "5px 10px" }} style={!tagState ? { maxHeight: "0px" } : {}}>
+                                {(Tags || []).map((value, index) => (
+                                    <Tag className="Tag" onClick={test[index]} >{value}</Tag>
+                                ))}
+                            </div>
+                        ) : (<></>)
+                    }
                 </div>
             </div>
-        </Wrapper>
+        </Wrapper >
     )
 }
 
